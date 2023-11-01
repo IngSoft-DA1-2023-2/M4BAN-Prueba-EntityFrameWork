9f9729e6dc81ee2092fadec91d7f107cca242554
Lucas Medina <lucasmedina180@gmail.com>
2023-11-01 08:45:22 -0300

    Created migrations

---
+++ DataAccess/EjercicioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class EjercicioDbContext : DbContext
+    {
+        public DbSet<Proyecto> Proyectos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<CargaHoras> CargasDeHoras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= .\\SQLEXPRESS;Database=EjercicioDbContext;Integrated Security=True;Trusted_Connection=True;Encrypt=False;");
+        }
+    }
+}
