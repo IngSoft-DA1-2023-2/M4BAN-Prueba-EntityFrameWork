54cd5fdfe9829e29b266567aa8483d07cae02d70
Mateo Falco <mateofalsar21@gmail.com>
2023-11-01 09:34:41 -0300

    Ejercicio2 Mateo Falco 283972

---
+++ ConsoleApp1/class.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Identity.Client;
+
+using (var context = new Ejercicio())
+{
+
+	var proyecto1 = new Proyecto { Nombre = "Proyecto1" };
+	var proyecto2 = new Proyecto { Nombre = "Proyecto2" };
+
+	context.Proyectos.Add(proyecto1);
+	context.Proyectos.Add(proyecto2);
+
+
+	var usuario1 = new Usuario { Nombre = "Usuario1", Proyecto = proyecto1 };
+	var usuario2 = new Usuario { Nombre = "Usuario2", Proyecto = proyecto2 };
+	context.Usuarios.Attach(usuario1);
+	context.Usuarios.Attach(usuario2);
+	context.Usuarios.Add(usuario1);
+	context.Usuarios.Add(usuario2);
+
+	context.CargaHoras.AddRange(
+		new CargaHoras { Fecha = DateTime.Now, Horas = 8, Descripcion = "Trabajo1", Usuario = usuario1 },
+		new CargaHoras { Fecha = DateTime.Now, Horas = 6, Descripcion = "Trabajo1", Usuario = usuario2 }
+	);
+	context.SaveChanges();
+}
