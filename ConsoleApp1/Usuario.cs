4eb79028ddac18b20a6edffe12c4ee475a76d56d
Santiago <st281306@fi365.ort.edu.uy>
2023-11-01 09:54:14 -0300

    Santiago Trinidad 281306

---
+++ ConsoleApp1/Usuario.cs
@@ -0,0 +1,8 @@
+public class Usuario
+{
+    public int UsuarioId { get; set; }
+    public string Nombre { get; set; }
+    public int ProyectoId { get; set; }
+    public virtual Proyecto Proyecto { get; set; }
+    public virtual ICollection<CargaHoras> CargasHoras { get; set; }
+}
